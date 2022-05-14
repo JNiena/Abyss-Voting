ca15dac82a0c5d8965d70b67f87ee466e7cc7f51
JNiena <JNiena@protonmail.com>
2022-05-14 12:43:25 -0400

    Reformat

--- source/Main.ts
+++ source/Main.ts
@@ -1,7 +1,7 @@
-import {LogLevel, SapphireClient} from '@sapphire/framework';
+import {LogLevel, SapphireClient} from "@sapphire/framework";
 import {Config} from "./Config";
 
 const config: Config = new Config("config.json");
-const client: SapphireClient = new SapphireClient({intents: ['GUILDS', 'GUILD_MESSAGES'], logger: {level: LogLevel.Debug}});
+const client: SapphireClient = new SapphireClient({intents: ["GUILDS", "GUILD_MESSAGES"], logger: {level: LogLevel.Debug}});
 
 client.login(config.get()["token"]).then();
