093b8bd0b642470817cec1b8eaf0fafb594c3c77
JNiena <JNiena@protonmail.com>
2022-05-14 12:24:55 -0400

    Now uses the json config

--- source/index.ts
+++
@@ -1,7 +0,0 @@
-import { LogLevel, SapphireClient } from '@sapphire/framework';
-import Config from './Config';
-
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'], logger: {level: LogLevel.Debug}});
-
-
-client.login(Config.token);
